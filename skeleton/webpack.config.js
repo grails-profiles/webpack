309aa9019560c9fee1f35b1d9ba4dae99888dd06
Zachary Klein <zak@silver-chalice.com>
2016-09-22 19:34:26 -0700

    Updated dependencies, moved source from main/js to main/webapp

--- skeleton/webpack.config.js
+++ skeleton/webpack.config.js
@@ -2,7 +2,7 @@ var path = require('path');
 
 module.exports = {
     entry: {
-        index: './src/main/js/index.js'
+        index: './src/main/webapp/index.js'
     },
     output: {
         path: './grails-app/assets/javascripts',
