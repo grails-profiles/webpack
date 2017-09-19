a4676d373093b758c3dd2fb0360c982e4b0963ce
sdelamo <sergio.delamo@softamo.com>
2017-09-19 06:29:42 +0200

    Remove files in docs branch

--- features/babel/skeleton/src/main/webapp/index.js
+++
@@ -1,1 +0,0 @@
-document.getElementById("app").innerHTML = "<p>Calling <code>repeat(3)</code> (ES6 via Babel)</p>".repeat(3);
--- skeleton/src/main/webapp/index.js
+++
@@ -1,1 +0,0 @@
-document.getElementById("app").innerHTML="<p>Rendered webpack bundle</p>";
